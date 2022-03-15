c55a6051632ede64ff8ba8d71c8c2065f1b39b01
KFella64 <79869527+KFella64@users.noreply.github.com>
2022-03-15 16:34:42 -0400

    Initial commit

---
+++ CampsiteReservationsApi/Services/LocalStatusService.cs
@@ -0,0 +1,30 @@
+﻿using CampsiteReservationsApi.Models;
+
+namespace CampsiteReservationsApi.Services;
+
+public class LocalStatusService : ICheckTheStatus
+{
+    private readonly ISystemTime _systemTime;
+    private readonly ILookupOnCallDevelopers _onCallDeveloperLookup;
+
+    public LocalStatusService(ISystemTime systemTime, ILookupOnCallDevelopers onCallDeveloperLookup)
+    {
+        _systemTime = systemTime;
+        _onCallDeveloperLookup = onCallDeveloperLookup;
+    }
+
+    public async Task<GetStatusResponse> GetCurrentStatusAsync()
+    {
+        var emailAddress = "";
+        try
+        {
+             emailAddress = await _onCallDeveloperLookup.GetEmailAddressAsync();
+        }
+        catch (UnableToProvideOnCallDeveloperException)
+        {
+
+            emailAddress = "laura@aol.com";
+        }
+        return new GetStatusResponse("Looks Good", emailAddress, _systemTime.GetCurrent());
+    }
+}
